7942f043885ed4c94307a09048d65fef28060ef1
Gonzalo Pérez de Eulate <g.p.e.140@gmail.com>
2024-05-24 22:47:07 -0300

    actualizado 2

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+// COMENTARIOS CLIENTES Open
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+// COMENTARIOS CLIENTES Fin
+
 
 // +++++++++++++++++++++++++++ 
 // Open VALIDACION FORMULARIO DE CONTACTO
@@ -61,3 +92,4 @@ formRegistro.addEventListener("submit", validarDatos);
 
 // End VALIDACION FORMULARIO DE CONTACTO 
 // +++++++++++++++++++++++++++ 
+
